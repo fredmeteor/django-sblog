d0ebbd48afc7d72dbe0f2823633816394272c901
fredmeteor <fredpeter09899@gmail.com>
2024-08-29 22:54:01 +0300

    Remove .pyc files from tracking

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 class PostListView(ListView):
@@ -14,28 +16,45 @@ class PostListView(ListView):
 
 
 
-def post_list(request):
-    post_list = Post.published.all()
+# class PostListView(ListView):
+#     model = Post
+#     template_name = 'blog/post/list.html'
+#     context_object_name = 'posts'
+#     paginate_by = 3  # Number of posts per page
+
+#     def get_queryset(self):
+#         # Return the queryset of published posts
+#         return Post.published.all()
+
+#     # Optionally, if you need to customize pagination behavior, you can override get_context_data
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         # Add extra context if needed
+#         return context
+
+
+# def post_list(request):
+#     post_list = Post.published.all()
     
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
     
-    try:
-      posts = paginator.page(page_number)
-    except PageNotAnInteger:
+#     try:
+#       posts = paginator.page(page_number)
+#     except PageNotAnInteger:
     
-     posts = paginator.page(1)
+#      posts = paginator.page(1)
     
-    except EmptyPage:
-      posts = paginator.page(paginator.num_pages)
+#     except EmptyPage:
+#       posts = paginator.page(paginator.num_pages)
       
     
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
 
 
 
@@ -52,4 +71,51 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+    
+    
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']],
+            )
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        },
     )
