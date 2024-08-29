17c2ca331e379984adf02a0242de4409a7c48c38
fredmeteor <fredpeter09899@gmail.com>
2024-08-29 19:52:44 +0300

    Modified post/list to CBV and url to suit and also the pagination

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+
+
+   queryset = Post.published.all()
+   context_object_name = 'posts'
+   paginate_by = 3
+   template_name = 'blog/post/list.html'
 
 
 
