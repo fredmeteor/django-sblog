e49786b106629cc8859a926eb15bd519242c76d7
fredmeteor <fredpeter09899@gmail.com>
2024-08-29 18:12:13 +0300

    Edited out unnecessary debug commentss in the blog/views

--- blog/views.py
+++ blog/views.py
@@ -14,29 +14,6 @@ def post_list(request):
 
 
 
-
-# def post_list(request):
-   
-#         posts = Post.published.all()  # Fetch all published posts
-#         return render(request, 'blog/post/list.html', {'posts': posts})  # Render the template with posts
-  
-
-
-# def post_list(request):
-#     try:
-#         posts = Post.published.all()
-#         return render(request, 'blog/post/list.html', {'posts': posts})
-#     except Exception as e:
-#         # Debugging output
-#         print(f"Error in post_list view: {e}")
-#         # Optionally handle the error or log it
-#         return render(request, 'blog/post/list.html', {'posts': []})  # Return an empty list or a custom error page
-
-
-
-
-
-
 def post_detail(request, id):
     post = get_object_or_404(
         Post,
